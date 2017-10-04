606a52371b4d520de88b36b73d524e8b70e4e277
Stas Vilchik <stas.vilchik@sonarsource.com>
2017-10-04 13:54:38 +0200

    wip

--- src/another-unused-import.js
+++ src/another-unused-import.js
@@ -1,4 +1,19 @@
 import A from 'a';
-A+1
+A + 1;
 console.log('A is not used!');
-A+1
+A + 1;
+
+function fn(param = () => true) {
+  param(1);
+  param(2);
+  param(3);
+  param(4);
+  param(5);
+  param(6);
+  param(7);
+  param(8);
+  param(9);
+  param(10);
+  param(11);
+  param(12);
+}
