1174ba4ff649ca2c47225c5733520e634ab304b6
Stas Vilchik <stas.vilchik@sonarsource.com>
2017-10-04 13:54:00 +0200

    wip

--- src/function-with-default-parameter.js
+++ src/function-with-default-parameter.js
@@ -1,3 +1,14 @@
-function fn (param = () => true) {
-  param(1)
+function fn(param = () => true) {
+  param(1);
+  param(2);
+  param(3);
+  param(4);
+  param(5);
+  param(6);
+  param(7);
+  param(8);
+  param(9);
+  param(10);
+  param(11);
+  param(12);
 }
