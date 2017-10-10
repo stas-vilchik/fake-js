ad996cd15ab39036da4c972e9d8b69f2d68c93da
Stas Vilchik <stas.vilchik@sonarsource.com>
2017-10-10 16:04:15 +0200

    add file with many issues on the same line

---
+++ src/many-issues-on-same-line.js
@@ -0,0 +1,3 @@
+if (false) {} if (false) {} if (false) {} if (false) {} if (false) {} if (false) {} if (false) {} if (false) {} if (false) {} if (false) {} if (false) {} if (false) {} if (false) {} if (false) {} if (false) {} if (false) {} if (false) {} if (false) {} if (false) {} if (false) {} if (false) {} if (false) {} if (false) {} if (false) {} if (false) {} if (false) {} if (false) {} if (false) {} if (false) {} if (false) {}
+if (true) {}
+if (true) {}
