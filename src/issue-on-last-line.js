0367da192ec897f4e857915e83bb26617326f706
Stas Vilchik <stas.vilchik@sonarsource.com>
2017-10-16 10:42:09 +0200

    add issue on last line

---
+++ src/issue-on-last-line.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import SeverityIcon from './SeverityIcon';
+import { translate } from '../../helpers/l10n';
+
+export default function SeverityHelper(props /*: { severity: ?string, className?: string } */) {
+  const { severity } = props;
+  if (!severity) {
+    return null;
+  }
+  return (
+    <span className={props.className}>
+      <SeverityIcon className="little-spacer-right" severity={severity} />
+      {translate('severity', severity)}
+    </span>
+  );
+}
+
+if (true) {}
